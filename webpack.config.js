5846b209ecb0306f5f66501e46bb1728ef785c98
Kevin Sperque <ferreirasperque.kevin@gmail.com>
2026-05-04 22:12:49 -0300

    Intalando Webpack Server

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,13 @@ entry: path.resolve(__dirname, "src", "main.js"),
 output: {
   filename: "main.js",
   path: path.resolve(__dirname, "dist"),
-}
+  },
+
+  devServer: {
+    static: [{
+      directory: path.join(__dirname, "dist")}],
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
 }
