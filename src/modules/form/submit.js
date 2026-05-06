e9da6dd96b310a4b4f1b56387419e6c6d2fc8645
Kevin Sperque <ferreirasperque.kevin@gmail.com>
2026-05-05 23:06:29 -0300

    Evento de carregamento do conteúdo

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,14 +1,14 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form") 
-const selectDate = document.getElementById("date")
+const selectedDate = document.getElementById("date")
 
 // Data atual para formatar o input.
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
 
 // Carrega a data atual e defini a data mínima como sendo a atual.
-selectDate.value = inputToday
-selectDate.min = inputToday
+selectedDate.value = inputToday
+selectedDate.min = inputToday
 
 form.onsubmit = (event) => {
   // Prevenir o comportamento padrão de carregar a página.
