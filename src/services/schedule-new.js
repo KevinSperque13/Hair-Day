026d281642c1f5d7576d6e9a965fc493395a3f96
Kevin Sperque <ferreirasperque.kevin@gmail.com>
2026-05-08 22:36:22 -0300

    Serviço para buscar agendamentos do dia

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,14 +2,16 @@ import { apiConfig } from "./api-config.js"
 
 export async function scheduleNew({ id, name, when }) {
   try {
+    // Faz a requisição para enviar os dados do agendamento.
     await fetch(`${apiConfig.baseURL}/schedules`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ id, name, when}),
+      body: JSON.stringify({ id, name, when }),
     })
-
+    
+    // Exibe mensagem de agendamento de realizado.
     alert("Agendamento realizado com sucesso")
   } catch (error) {
     console.log(error)
